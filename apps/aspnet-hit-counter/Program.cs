7c2df586ff47b577ae625ef5f2162b36ef463044
Will <williampsena@gmail.com>
2023-03-27 21:20:54 -0300

    Add nginx sample

--- apps/aspnet-hit-counter/Program.cs
+++ apps/aspnet-hit-counter/Program.cs
@@ -1,11 +1,12 @@
 var hits = 0;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var hostName = System.Net.Dns.GetHostName();
 
 app.MapGet("/", () =>
 {
     hits++;
-    return $"You have hit this server {hits} time(s).";
+    return $"You have hit this server ({hostName}) {hits} time(s).";
 });
 
 app.Run();
